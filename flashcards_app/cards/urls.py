d9ed18394c6491f7f2985e12216bb88f1e6ec425
Lone Hilke <lonehilke@gmail.com>
2022-08-08 22:21:03 +0200

    kommet til step 4
    Co-Authored-By: Qvisty <18654175+qvisty@users.noreply.github.com>

--- flashcards_app/cards/urls.py
+++ flashcards_app/cards/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import Home
+#from django.views.generic import TemplateView
+from . import views
 
 urlpatterns = [
     #path("", TemplateView.as_view(template_name="cards/base.html"), name="home"),
-    path("", Home.as_view(), name="home"),
+    
+    path("", views.CardListView.as_view(), name="card-list"),
 ]
--- flashcards_app/cards/views.py
+++ flashcards_app/cards/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
 from .models import Card
 from django.views import View
+from django.views.generic import (
+    ListView,
+)
 
 # Create your views here.
-class Home(View):
-    def get(self, request):
-        return render(request, "cards/base.html")
+#class Home(View):
+    #def get(self, request):
+        #return render(request, "cards/base.html")
+
+class CardListView(ListView):
+    model = Card
+    queryset = Card.objects.all().order_by("box", "-date_created")
